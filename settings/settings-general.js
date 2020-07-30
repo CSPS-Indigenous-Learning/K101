6023e13af8f94bc61f833b1d7131d8695dd23296
Benoit Lord <benoit.lord@canada.ca>
2020-07-30 14:37:51 -0400

    First commit from TFS cs3598

---
+++ settings/settings-general.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+define({
+
+
+	/*------------------ Course info -----------------*/
+	courseLegacyCode:"K101",
+	seriesTitle_en: "K101",
+	seriesTitle_fr: "K101",
+	courseTitle_en: "First Nations in Canada",
+	courseTitle_fr: "Les Premières Nations au Canada",
+
+	showGlossary: true,
+	triggerCompletionWhenAllPagesViewed: true,
+	
+	"googleAnalyticsID": "UA-101907595-22"
+});
